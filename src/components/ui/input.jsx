1a4de4d49c65cbf911a79ba6c9a997f9ee2fc8d9
Marielimbis <myf.mariel@gmail.com>
2026-02-08 15:33:09 +0100

    Add Input component for user input fields

---
+++ src/components/ui/input.jsx
@@ -0,0 +1,8 @@
+export function Input(props) {
+  return (
+    <input
+      {...props}
+      className="h-10 w-full rounded-md border border-neutral-300 px-3 text-sm focus:outline-none"
+    />
+  );
+}
